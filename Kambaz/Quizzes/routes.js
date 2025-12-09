3f5593f6af5e2e669ca115d49e81da4449447dea
Ruoxi Wang <ruoxiwang42@gmail.com>
2025-12-09 10:41:32 -0500

    final

--- Kambaz/Quizzes/routes.js
+++ Kambaz/Quizzes/routes.js
@@ -59,13 +59,21 @@ export default function QuizRoutes(app) {
   
 
   app.get("/api/quizzes/:quizId", async (req, res) => {
+    const user = req.session["currentUser"];
     const quiz = await quizDao.findQuizById(req.params.quizId);
-  
-    if (!quiz) {
-      return res.sendStatus(404);
-    }
-    
-    res.json(quiz);
+
+    if (!quiz) return res.sendStatus(404);
+
+    const isTeacher =
+      user?.role === "ADMIN" ||
+      user?.role === "FACULTY" ||
+      user?.role === "TA";
+
+    if (isTeacher) return res.json(quiz);
+
+    if (quiz.published === true) return res.json(quiz);
+
+    return res.status(403).json({ message: "Quiz not available" });
   });
 
   app.put("/api/quizzes/:quizId", async (req, res) => {
